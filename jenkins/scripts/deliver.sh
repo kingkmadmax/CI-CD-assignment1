9edb8e80f9566f5c1d120caf17caa9a7d1d5c8c0
kingkmademax <kirubelmamushet@gmail.com>
2026-01-26 07:04:27 +0300

    Fix deliver.sh to work with Vite React app

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory, correctly bundles React in'
-echo 'production mode and optimizes the build for the best performance.'
+echo 'The following "npm" command builds your React application for production'
 set -x
 npm run build
 set +x
 
-echo 'The following "npm" command runs your Node.js/React application in'
-echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
-echo 'the file ".pidfile".'
+echo 'The following command runs your React application using a simple HTTP server'
+echo 'to serve the built files from the dist directory.'
 set -x
-npm start &
-sleep 1
-echo $! > .pidfile
+npx --yes serve -s dist -l 3000 &
+pid=$!
+echo $pid > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo 'Visit http://localhost:3000 to see your React application in action.'
