21a721f99c2715d25a18234dbec4306ff0e26df3
kingkmademax <kirubelmamushet@gmail.com>
2026-01-26 07:22:20 +0300

    deployment Stage

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your React application for production'
+echo 'The following "npm" command builds your Node.js/React application for'
+echo 'production in the local "build" directory, correctly bundles React in'
+echo 'production mode and optimizes the build for the best performance.'
 set -x
 npm run build
 set +x
 
-echo 'The following command runs your React application using a simple HTTP server'
-echo 'to serve the built files from the dist directory.'
+echo 'The following "npm" command runs your Node.js/React application in'
+echo 'preview mode and makes the application available for web browsing.'
+echo 'The "npm run preview" command has a trailing ampersand so that the command runs'
+echo 'as a background process (i.e. asynchronously). Otherwise, this command'
+echo 'can pause running builds of CI/CD applications indefinitely.'
 set -x
-npx --yes serve -s dist -l 3000 &
-pid=$!
-echo $pid > .pidfile
+npm run preview -- --port 3000 --host &
+sleep 1
+echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your React application in action.'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
